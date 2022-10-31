edbbf241c123c379fc688e5160e0eccff591b7b3
邓进忠 <hn.dengjinzhong@aisino.com>
2022-10-31 17:02:06 +0800

    feat: electron 应用打包插件开发

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { devPlugin, getReplacer } from "./plugins/devPlugin";
+import { buildPlugin } from "./plugins/buildPlugin";
 import optimizer from "vite-plugin-optimizer";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [optimizer(getReplacer()),devPlugin(), vue()]
+  plugins: [optimizer(getReplacer()),devPlugin(), vue()],
+  build: {
+    rollupOptions: {
+      plugins: [buildPlugin()],
+    },
+  },
 })
